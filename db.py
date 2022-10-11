e5a760e2002a356de9602dd72af3eba2bc6fb9f8
zvic <zvic@list.ru>
2022-10-11 09:17:09 +0300

    Fixed #9 : add linting to vscode, used flake8

--- db.py
+++ db.py
@@ -1,79 +1,84 @@
-#not for use, test
-def connect_db(connection,host,user,password,db_name):
-    connection=psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=db_name
-        )
+# not for use, test
+def connect_db(connection, host, user, password, db_name):
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=db_name
+    )
     connection.autocommit = True
-    
-    
-#select * records from db and return dictionary   
+
+
+# select * records from db and return dictionary
 def select_all_db(connection):
     with connection.cursor() as g:
         g.execute("SELECT id, name FROM goods ORDER BY id;")
-        cnt=g.fetchall()
-        goods=[]
+        cnt = g.fetchall()
+        goods = []
         for row in cnt:
-            good={
-                'id' : row[0],
-                'name' : row[1],
+            good = {
+                'id': row[0],
+                'name': row[1],
             }
             goods.append(good)
         return goods
 
-#select id records from db and return dictionary   
-def select_id_db(connection,id):
+# select id records from db and return dictionary
+
+
+def select_id_db(connection, id):
     with connection.cursor() as g:
-        g.execute("SELECT * FROM goods WHERE id=%s;",(id,))
-        cnt=g.fetchall()
-        goods=[]
+        g.execute("SELECT * FROM goods WHERE id=%s;", (id,))
+        cnt = g.fetchall()
+        goods = []
         for row in cnt:
-            good={
-                'id' : row[0],
-                'name' : row[1],
-                'price' : row[2],
-                'manufacture_date' : row[3],
-                'picture_url' : row[4]               
+            good = {
+                'id': row[0],
+                'name': row[1],
+                'price': row[2],
+                'manufacture_date': row[3],
+                'picture_url': row[4]
             }
             goods.append(good)
         return goods
 
 # insert new good into db
-def insert_db(connection,good_list):
+
+
+def insert_db(connection, good_list):
     with connection.cursor() as g:
         g.execute("""
                     INSERT INTO goods (name,price,manufacture_date,picture_url)
-    VALUES (%s,%s,%s,%s);""",(good_list[0],good_list[1],good_list[2],good_list[3]))
+    VALUES (%s,%s,%s,%s);""", (good_list[0], good_list[1], good_list[2], good_list[3]))
         g.execute("SELECT id FROM goods ORDER BY id DESC LIMIT 1;")
-        cnt=g.fetchone()
+        cnt = g.fetchone()
     return cnt
 
 # update goods where id
-def update_id_db(connection,id,good_list):
+
+
+def update_id_db(connection, id, good_list):
     with connection.cursor() as g:
-        g.execute("SELECT COUNT(id) FROM goods WHERE id=%s",(id,))
-        cnt=g.fetchone()
+        g.execute("SELECT COUNT(id) FROM goods WHERE id=%s", (id,))
+        cnt = g.fetchone()
         if not cnt[0]:
             return 0
         g.execute("""
                     UPDATE goods SET name=%s,price=%s,manufacture_date=%s,
-                    picture_url=%s WHERE id=%s;""", (good_list[0], good_list[1], good_list[2],good_list[3],id))
-    return 1   
-    
+                    picture_url=%s WHERE id=%s;""", (good_list[0], good_list[1], good_list[2], good_list[3], id))
+    return 1
+
 # delete good=id
+
+
 def delete_id_db(connection, id):
     with connection.cursor() as g:
         g.execute("""
-                    DELETE FROM goods WHERE id=%s;""",(id,))
-    return g.statusmessage    
-    
-         
+                    DELETE FROM goods WHERE id=%s;""", (id,))
+    return g.statusmessage
+
+
 def close_db(connection):
     if connection:
         connection.close()
         print("<connection closed>")
-    
-         
-    
--- main.py
+++ main.py
@@ -1,72 +1,78 @@
-from flask import Flask,jsonify,abort,request,render_template
+from flask import Flask, jsonify, abort, request, render_template
 import psycopg2
-from config import host,user,password,db_name #config for database postgres
+from config import host, user, password, db_name  # config for database postgres
 # db.py - functions for bd access - select_all_db(connection),select_id_db(connection,id)
-#insert_db(connection,good_list),update_id_db(connection,id,good_list),delete_id_db(connection,id),close_db(connection)
+# insert_db(connection,good_list),update_id_db(connection,id,good_list),delete_id_db(connection,id),close_db(connection)
 import db
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-connection=psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=db_name
-        )
+connection = psycopg2.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=db_name
+)
 connection.autocommit = True
+
 
 @app.route('/')
 def index():
     return render_template('index.html', name='vzaharov')
 
+
 @app.route('/goods', methods=['GET'])
 def get_goods():
-    goods=db.select_all_db(connection)
-    return jsonify(goods),200
+    goods = db.select_all_db(connection)
+    return jsonify(goods), 200
+
 
 @app.route('/goods/<int:good_id>', methods=['GET'])
 def get_good_id(good_id):
-    good=db.select_id_db(connection,good_id)
-    if len(good)==0:
-        return jsonify({'error':'no id '+str(good_id)}),404
-    return jsonify(good),200
+    good = db.select_id_db(connection, good_id)
+    if len(good) == 0:
+        return jsonify({'error': 'no id '+str(good_id)}), 404
+    return jsonify(good), 200
+
 
 @app.route('/goods', methods=['POST'])
 def create_good():
-    if not request.get_json(silent=True,force=True):
-        return jsonify({'error':'no json'}),400
-    good=[
+    if not request.get_json(silent=True, force=True):
+        return jsonify({'error': 'no json'}), 400
+    good = [
         request.json['name'],
-        request.json.get('price',""),
-        request.json.get('manufacture_date',""),
-        request.json.get('picture_url',"")
+        request.json.get('price', ""),
+        request.json.get('manufacture_date', ""),
+        request.json.get('picture_url', "")
     ]
-    res=db.insert_db(connection,good)
-    return jsonify({'id':res[0]}),201
+    res = db.insert_db(connection, good)
+    return jsonify({'id': res[0]}), 201
+
 
 @app.route('/goods/<int:good_id>', methods=['PUT'])
 def put_good_id(good_id):
-    if not request.get_json(silent=True,force=True):
-        return jsonify({'error':'no json'}),400
-    good=[
+    if not request.get_json(silent=True, force=True):
+        return jsonify({'error': 'no json'}), 400
+    good = [
         request.json['name'],
-        request.json.get('price',""),
-        request.json.get('manufacture_date',""),
-        request.json.get('picture_url',"")
+        request.json.get('price', ""),
+        request.json.get('manufacture_date', ""),
+        request.json.get('picture_url', "")
     ]
-    res=db.update_id_db(connection,good_id,good)
+    res = db.update_id_db(connection, good_id, good)
     if not res:
-        return jsonify({'error':'no id '+str(good_id)}),404        
-    return '',200
+        return jsonify({'error': 'no id '+str(good_id)}), 404
+    return '', 200
+
 
 @app.route('/goods/<int:good_id>', methods=['DELETE'])
 def delete_good_id(good_id):
-    res=db.delete_id_db(connection,good_id)
-    if res[-1]=='0':
-        return jsonify({'error':'no id '+str(good_id)}),404
-    return '',200
+    res = db.delete_id_db(connection, good_id)
+    if res[-1] == '0':
+        return jsonify({'error': 'no id '+str(good_id)}), 404
+    return '', 200
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
