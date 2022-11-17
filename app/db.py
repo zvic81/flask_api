4830158a3b23d9015298efb567b0ab6783b233ca
vic <zvic@list.ru>
2022-11-17 16:29:30 +0300

    Fixed #12 Fixed#18 Made endpoint incoming orders; made function for connect DB; put Schemas for ApiFlask in file schemas.py

--- app/db.py
+++ app/db.py
@@ -1,39 +1,48 @@
-# not for use, test
-def connect_db(connection, host, user, password, db_name):
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name
-    )
+import psycopg2
+# config for database postgres
+from config import host, user, password, db_name, port
+
+
+def connect_db(host=host, user=user, password=password, db_name=db_name):
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name,
+            port=port
+        )
+    except psycopg2.OperationalError:
+        print('Error: No BD server ready, try one more')
+        return 0
+        # sleep(0.1)  # wait 1sec to try connect
     connection.autocommit = True
+    return connection
 
 
 # select * records from db and return list
-def select_all_goods_db(connection: any) -> list:
+def select_all_goods_db() -> list:
+    connection = connect_db()
     with connection.cursor() as g:
         g.execute("SELECT id, name FROM goods ORDER BY id;")
         cnt = g.fetchall()
         goods = []
         for row in cnt:
-            good = {
-                'id': row[0],
-                'name': row[1],
-            }
-            goods.append(good)
+            goods.append({'id': row[0], 'name': row[1], })
+        close_db(connection)
         return goods
 
-#  select all orders from db, return list of dictionaries
 
-
-def select_all_orders_db(connection: any) -> list:
+# select all orders from db, return list of dictionaries
+def select_all_orders_db() -> list:
+    connection = connect_db()
     with connection.cursor() as g:
         g.execute(
             "SELECT id, order_date, customer_name, customer_email, delivery_address, status, notes FROM orders ORDER BY id;")
         cnt = g.fetchall()
         orders = []
         for row in cnt:
-            order = {
+            orders.append({
                 'id': row[0],
                 'order_date': row[1],
                 'customer_name': row[2],
@@ -41,42 +50,46 @@ def select_all_orders_db(connection: any) -> list:
                 'delivery_address': row[4],
                 'status': row[5],
                 'notes': row[6],
-            }
-            orders.append(order)
+            })
+        close_db(connection)
         return orders
 
 
 # select id records from db and return list
-def select_id_good_db(connection: any, id: int) -> list:
+def select_id_good_db(id: int) -> list:
+    connection = connect_db()
     with connection.cursor() as g:
         g.execute("SELECT * FROM goods WHERE id=%s;", (id,))
         cnt = g.fetchall()
         goods = []
         for row in cnt:
-            good = {
+            goods.append({
                 'id': row[0],
                 'name': row[1],
                 'price': row[2],
                 'manufacture_date': row[3],
                 'picture_url': row[4]
-            }
-            goods.append(good)
+            })
+        close_db(connection)
         return goods
 
 # insert new good into db
 
 
-def insert_good_db(connection: any, good_list: list) -> tuple:
+def insert_good_db(good_list: dict) -> list:
+    connection = connect_db()
     with connection.cursor() as g:
         g.execute("""
-                    INSERT INTO goods (name,price,manufacture_date,picture_url)
-    VALUES (%s,%s,%s,%s);""", (good_list[0], good_list[1], good_list[2], good_list[3]))
+                    INSERT INTO goods (name, price, manufacture_date, picture_url)
+        VALUES (%s,%s,%s,%s);""", (good_list.get('name'), good_list.get('price'), good_list.get('manufacture_date'), good_list.get('picture_url')))
         g.execute("SELECT id FROM goods ORDER BY id DESC LIMIT 1;")
         cnt = g.fetchone()
-    return cnt
+    close_db(connection)
+    return cnt[0]
 
 
-def insert_order_db(connection: any, order_list: dict) -> int:
+def insert_order_db(order_list: dict) -> int:
+    connection = connect_db()
     with connection.cursor() as g:
         g.execute("""
                 INSERT INTO orders (order_date, customer_name, customer_email, delivery_address, status, notes)
@@ -89,34 +102,35 @@ def insert_order_db(connection: any, order_list: dict) -> int:
             g.execute("""
                      INSERT INTO order_item (ammount, notes, order_id, good_id)
                      VALUES (%s,%s,%s,%s);""", (item.get('ammount'), 'notes null', cnt[0], item.get('good_id')))
-
+    close_db(connection)
     return cnt[0]  # return id new order
 
-# update goods where id
 
-
-def update_id_good_db(connection: any, id: int, good_list: list) -> int:
+def update_id_good_db(id: int, good_list: list) -> int:
+    connection = connect_db()
     with connection.cursor() as g:
         g.execute("SELECT COUNT(id) FROM goods WHERE id=%s", (id,))
         cnt = g.fetchone()
         if not cnt[0]:
+            close_db(connection)
             return 0
         g.execute("""
                     UPDATE goods SET name=%s,price=%s,manufacture_date=%s,
-                    picture_url=%s WHERE id=%s;""", (good_list[0], good_list[1], good_list[2], good_list[3], id))
+                    picture_url=%s WHERE id=%s;""", (good_list.get('name'), good_list.get('price'), good_list.get('manufacture_date'), good_list.get('picture_url'), id))
+    close_db(connection)
     return 1
 
-# delete good=id
 
-
-def delete_id_good_db(connection: any, id: int) -> str:
+def delete_id_good_db(id: int) -> str:
+    connection = connect_db()
     with connection.cursor() as g:
         g.execute("""
                     DELETE FROM goods WHERE id=%s;""", (id,))
+    close_db(connection)
     return g.statusmessage
 
 
 def close_db(connection: any) -> None:
     if connection:
         connection.close()
-        print("<connection closed>")
+        # print("<connection closed>")
---
+++ app/schemas.py
@@ -0,0 +1,58 @@
+from apiflask import Schema, fields
+# from apiflask.fields import Integer, String, Field, Date, List, Nested
+from apiflask.validators import Range
+
+
+class BaseResponse(Schema):
+    data = fields.Field()  # the data key
+    code = fields.Integer()
+
+
+class GoodsOut(Schema):  # list of short goods for response
+    id = fields.Integer(metadata={'example': 2})
+    name = fields.String(metadata={'example': 'Coffee'})
+
+
+class GoodIn(Schema):  # one good with full fields
+    name = fields.String(required=True, metadata={'example': 'Coffee'})
+    price = fields.Integer(metadata={'example': 21})
+    manufacture_date = fields.Date(metadata={'example': '2019-02-05'})
+    picture_url = fields.String(metadata={'example': 'pic.com/mypic.jpg'})
+
+
+class GoodOut(GoodIn):  # one good with full fields
+    id = fields.Integer()
+
+
+class OrdersOut(Schema):    # list of orders for responce
+    id = fields.Integer()
+    order_date = fields.Date()
+    customer_name = fields.String()
+    customer_email = fields.String()
+    delivery_address = fields.String()
+    status = fields.String()
+    notes = fields.String()
+
+    class Meta:
+        ordered = True
+
+
+class OrderGood(Schema):
+    good_id = fields.Integer(required=True, validate=Range(
+        min=1), metadata={'example': 1})
+    ammount = fields.Integer(required=True, validate=Range(
+        1, 1000), metadata={'example': 2})
+
+
+class OrderIn(Schema):
+    order_date = fields.Date(required=True, metadata={'example': '2022-11-05'})
+    customer_name = fields.String(
+        required=True, metadata={'example': 'Carlson'})
+    customer_email = fields.String(metadata={'example': 'carlson@gmail.com'})
+    delivery_address = fields.String(metadata={'example': 'Apatity'})
+    notes = fields.String(metadata={'example': 'poor customer'})
+    good_item = fields.List(fields.Nested(OrderGood()), required=True)
+
+
+class MessageOk(Schema):
+    id = fields.Integer()
